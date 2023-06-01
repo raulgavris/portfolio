56469d0314996c4e5c7d1c99fb217c53551e3549
Raul Gavris <rg.raulgavris@gmail.com>
2023-06-01 18:54:42 +0300

    blog page

---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,62 @@
+import { LikesSvg, ViewsSvg } from './Svg'
+
+const Button1 = () => {
+  return (
+    <div className="flex h-[200px] w-[500px] flex-col items-start justify-between rounded-xl bg-darkGray px-6 py-4 drop-shadow-basic">
+      <div className="flex w-full flex-row items-center justify-between">
+        <div className="font-raulmono text-3xl text-lightGray">
+          Blog Title 1
+        </div>
+        <div className="font-raulmono text-xs text-lightGray">5 mins read</div>
+      </div>
+      <div className="w-10/12 text-xs text-offWhite">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum,
+        tortor ac ultrices malesuada, metus libero bibendum justo, non finibus
+        tortor turpis id tortor. Mauris quis ex ut velit commodo scelerisque . .
+        .
+      </div>
+      <div className="flex w-full flex-row items-center justify-between">
+        <div className="flex flex-col items-start justify-start">
+          <div className="flex w-24 flex-row items-center justify-start gap-2">
+            <div className="text-xs text-lightGray">views:</div>
+            <div className="text-xs text-red">26</div>
+            <ViewsSvg className="mb-1 h-3.5 w-3.5 fill-softOrange" />
+          </div>
+          <div className="mt-1 flex w-24 flex-row items-center justify-start gap-2">
+            <div className="text-xs tracking-[1px] text-lightGray">likes:</div>
+            <div className="text-xs text-red">18</div>
+            <LikesSvg className="mb-1 h-3.5 w-3.5 fill-softGreen" />
+          </div>
+        </div>
+        <div className="flex flex-row items-start justify-start gap-2">
+          <div className="flex h-7 w-10 flex-row items-center justify-center rounded-sm bg-teal font-raulmono text-xs text-offWhite drop-shadow-basic">
+            git
+          </div>
+          <div className="flex h-7 w-10 flex-row items-center justify-center rounded-sm bg-darkBlack font-raulmono text-xs text-offWhite drop-shadow-basic">
+            web
+          </div>
+          <div className="flex h-7 w-10 flex-row items-center justify-center rounded-sm bg-softOrange font-raulmono text-xs text-offWhite drop-shadow-basic">
+            FE
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-start gap-1">
+          <div className="flex flex-col items-end justify-end">
+            <div className="font-raulmono text-xs text-lightGray">
+              Raul Gavriș
+            </div>
+            <div className="font-raulmono text-xs text-teal">
+              06-04-2023 22:24
+            </div>
+          </div>
+          <img
+            alt="blog_image"
+            src="/hero-large.png"
+            className="m-0 h-10 w-10 rounded-full p-0 shadow-inner drop-shadow-basic"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Button1
--- src/components/Sections/BlogSection.tsx
+++ src/components/Sections/BlogSection.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import BlogCard from '@/components/BlogCard'
 
 const BlogSection = ({ containerRef, blogRef }: BlogSectionProps) => {
   const router = useRouter()
+
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     const targetElement = document.getElementById('blog')
@@ -23,11 +26,36 @@ const BlogSection = ({ containerRef, blogRef }: BlogSectionProps) => {
       ref={blogRef}
       data-path="/blog"
     >
-      <div className="max-w-9xl grid h-full w-full grid-cols-12 grid-rows-12">
-        <div className="relative col-start-2 col-end-11 row-start-3 row-end-12 ml-10">
-          Raul
+      <div className="max-w-9xl flex h-full w-full flex-col items-center justify-center px-80 py-40">
+        <div className="grid h-full w-full grid-flow-col grid-cols-2 grid-rows-3 gap-10">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
         </div>
-        <div className="bottom-2 left-1/2 col-start-6 col-end-7 row-start-11 row-end-12 flex flex-row items-center justify-center gap-3"></div>
+        <div className="mousehover relative mt-16 flex w-1/2 flex-row items-center justify-center">
+          <input
+            maxLength={50}
+            required
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value)
+            }}
+            id="search"
+            name="search"
+            type="text"
+            placeholder="Search blog post by title, tags or content"
+            className={`peer relative w-full rounded-md px-3 py-4 text-sm placeholder-transparent drop-shadow-basic placeholder:text-xs focus:outline-none focus:ring-opacity-75 dark:bg-darkGray`}
+          />
+          <label
+            htmlFor="name"
+            className="pointer-events-none absolute -top-7 left-2 w-full text-sm opacity-100 transition-all duration-500 text-shadow-text peer-placeholder-shown:left-10 peer-placeholder-shown:top-2.5 peer-placeholder-shown:font-raulmono peer-placeholder-shown:text-xs peer-placeholder-shown:opacity-50"
+          >
+            Search blog post by title, tags or content
+          </label>
+        </div>
       </div>
     </div>
   )
