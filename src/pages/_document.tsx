7e69e597a8d17d6c7387fbb0f43639c33f3e136b
Raul Gavris <rg.raulgavris@gmail.com>
2023-06-21 12:56:53 +0300

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:image" content="/hero-large.png" />
+      </Head>
       <body>
         <div className="cursor"></div>
         <div className="cursor2"></div>
